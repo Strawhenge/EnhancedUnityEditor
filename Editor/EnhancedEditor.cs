6084623cd6b6659e574e8c4d31323427f6f90cde
Matt <matt_bristow@rocketmail.com>
2021-02-20 20:07:09 +0000

    created basic method call buttons

--- Editor/EnhancedEditor.cs
+++ Editor/EnhancedEditor.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor;
+﻿using System.Linq;
+using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 
 namespace EnhancedUnityEditor
@@ -6,6 +8,37 @@ namespace EnhancedUnityEditor
     [CustomEditor(typeof(MonoBehaviour), editorForChildClasses: true)]
     public class EnhancedEditor : Editor
     {
+        private MethodInfo[] publicMethods = new MethodInfo[0];
 
+        private void OnEnable()
+        {
+            var targetType = target.GetType();
+            var targetMethods = targetType.GetNonMonobehaviourMethods();
+
+            publicMethods = targetMethods.Where(x => x.IsPublic).ToArray();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            foreach (var method in publicMethods)
+            {
+                HandleMethod(method);
+            }
+        }
+
+        private void HandleMethod(MethodInfo method)
+        {
+            if (GUILayout.Button(method.Name))
+            {
+                var returnValue = method.Invoke(target, new object[0]);
+
+                if (method.ReturnType != typeof(void))
+                {
+                    Debug.Log(returnValue, target);
+                }
+            }
+        }
     }
 }
---
+++ Editor/TypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace EnhancedUnityEditor
+{
+    internal static class TypeExtensions
+    {
+        private static readonly Type[] monobehaviourHierarchyTypes = new Type[]
+        {
+            typeof(MonoBehaviour),
+            typeof(Behaviour),
+            typeof(Component),
+            typeof(Object),
+            typeof(object)
+        };
+
+        public static MethodInfo[] GetNonMonobehaviourMethods(this Type type)
+        {
+            return type
+                .GetMethods()
+                .Where(x => !monobehaviourHierarchyTypes.Contains(x.DeclaringType))
+                .ToArray();
+        }
+    }
+}
