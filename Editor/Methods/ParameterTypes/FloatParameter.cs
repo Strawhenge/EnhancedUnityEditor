608ad0c0f4a155bb58329edef64978c2b775d9f8
Matt <matt_bristow@rocketmail.com>
2021-02-21 21:40:31 +0000

    added more parameter types

---
+++ Editor/Methods/ParameterTypes/FloatParameter.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using UnityEditor;
+
+namespace EnhancedUnityEditor.Methods
+{
+    internal class FloatParameter : Parameter<float>
+    {
+        public FloatParameter(ParameterInfo parameterInfo) : base(parameterInfo)
+        {
+        }
+
+        protected override float Draw(float currentValue)
+        {            
+            return EditorGUILayout.FloatField(name, currentValue);
+        }
+    }
+}
---
+++ Editor/Methods/ParameterTypes/StringParameter.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using UnityEditor;
+
+namespace EnhancedUnityEditor.Methods
+{
+    internal class StringParameter : Parameter<string>
+    {
+        public StringParameter(ParameterInfo parameterInfo) : base(parameterInfo)
+        {
+        }
+
+        protected override string Draw(string currentValue)
+        {
+            return EditorGUILayout.TextField(name, currentValue);
+        }
+    }
+}
