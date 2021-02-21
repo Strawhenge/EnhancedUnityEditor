0d28c2118b6bb4a0f472191a5c984cbf339f9bcb
Matt <matt_bristow@rocketmail.com>
2021-02-21 22:05:21 +0000

    added more paramter types

--- Editor/Methods/Parameter.cs
+++ Editor/Methods/Parameter.cs
@@ -1,18 +1,34 @@
-﻿using System.Reflection;
-using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace EnhancedUnityEditor.Methods
 {
     internal abstract class Parameter
     {
+        private static readonly Dictionary<Type, Type> parameterTypes = new Dictionary<Type, Type>();
+
+        static Parameter()
+        {
+            var types = typeof(Parameter).Assembly
+                .GetTypes()
+                .Where(x => x.IsSubclassOf(typeof(Parameter)) && x.BaseType.IsGenericType);
+
+            parameterTypes = types.ToDictionary(
+                keySelector: x => x.BaseType.GenericTypeArguments.First(),
+                elementSelector: x => x);
+        }
+
         public static Parameter Create(ParameterInfo parameterInfo)
         {
             var type = parameterInfo.ParameterType;
 
-            if (type == typeof(Vector3))
-                return new Vector3Parameter(parameterInfo);
+            if (!parameterTypes.ContainsKey(type))
+                return new ObjectParameter(parameterInfo);
 
-            return new ObjectParameter(parameterInfo);
+            var parameterType = parameterTypes[type];
+            return (Parameter)Activator.CreateInstance(parameterType, new object[] { parameterInfo });
         }
 
         public abstract object CurrentValue { get; }
---
+++ Editor/Methods/ParameterTypes/BooleanParameter.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using UnityEditor;
+
+namespace EnhancedUnityEditor.Methods
+{
+    internal class BooleanParameter : Parameter<bool>
+    {
+        public BooleanParameter(ParameterInfo parameterInfo) : base(parameterInfo)
+        {
+        }
+
+        protected override bool Draw(bool currentValue)
+        {
+            return EditorGUILayout.Toggle(name, currentValue);
+        }
+    }
+}
