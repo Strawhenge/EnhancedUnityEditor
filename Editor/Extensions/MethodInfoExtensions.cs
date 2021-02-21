f1d0b2c0a4ee263fb22e01f572d122ccf50c4649
Matt <matt_bristow@rocketmail.com>
2021-02-21 21:17:19 +0000

    added some extensions

---
+++ Editor/Extensions/MethodInfoExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace EnhancedUnityEditor
+{
+    internal static class MethodInfoExtensions
+    {
+        public static bool HasReturnType(this MethodInfo methodInfo) =>
+            methodInfo.ReturnType != typeof(void);
+    }
+}
